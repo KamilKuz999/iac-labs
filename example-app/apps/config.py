87624b1f3e93a330cc372652ab61738dcfe128e7
KamilKuz999 <133576098+KamilKuz999@users.noreply.github.com>
2023-12-17 09:49:38 +0100

    Update config.py

--- example-app/apps/config.py
+++ example-app/apps/config.py
@@ -34,7 +34,7 @@ class ProductionConfig(Config):
         f"{os.getenv('DB_USERNAME', 'root')}:"
         f"{os.getenv('DB_URL', 'postgres://root:blI9WS5f13XLYsNIJ1namBwud0APer1a@dpg-cltvd821hbls73e7irgg-a/appseed_db_s2ws')}://"
         f"{os.getenv('DB_PASS', 'blI9WS5f13XLYsNIJ1namBwud0APer1a')}@"
-        f"{os.getenv('DB_HOST', 'dpg-cltvd821hbls73e7irgg-a')}:"
+        f"{os.getenv('DB_HOST', 'dpg-cltvd821hbls73e7irgg-a.oregon-postgres.render.com')}:"
         f"{os.getenv('DB_PORT', '5432')}/"
         f"{os.getenv('DB_NAME', 'appseed_db_s2ws')}?sslmode=require"
     )    
